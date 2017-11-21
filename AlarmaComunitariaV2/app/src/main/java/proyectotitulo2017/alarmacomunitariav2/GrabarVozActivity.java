00dbb66b7533e30be329de3fcffb4c9a3186b5df
Jorge <jorge.g.gallerstein@gmail.com>
2017-11-21 00:10:04 -0300

    Pantallas:
    Mensaje y Grabador de voz

--- AlarmaComunitariaV2/app/src/main/java/proyectotitulo2017/alarmacomunitariav2/GrabarVozActivity.java
+++ AlarmaComunitariaV2/app/src/main/java/proyectotitulo2017/alarmacomunitariav2/GrabarVozActivity.java
@@ -1,6 +1,5 @@
 package proyectotitulo2017.alarmacomunitariav2;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
@@ -8,32 +7,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MensajeActivity extends AppCompatActivity {
+public class GrabarVozActivity extends AppCompatActivity {
 
-    private Button grabadorVoz;
+
+    private ImageButton grabadorVoz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mensaje);
+        setContentView(R.layout.activity_grabar_voz);
     }
+
 
     public void onclickMsgAlerta(View view){
         alertOneButton();
     }
 
-    public void onClickScreenMsgVoz(View view){
-        grabadorVoz =(Button) findViewById(R.id.btnGrabadorVoz);
-        Intent intent = new Intent(MensajeActivity.this, GrabarVozActivity.class);
-        startActivity(intent);
-    }
-
     public void alertOneButton() {
 
-        AlertDialog alertDialog = new AlertDialog.Builder(MensajeActivity.this)
-                .setTitle("Alerta enviada")
-                .setMessage("Se ha enviado un mensaje a todos los usuarios de la comunidad")
+        AlertDialog alertDialog = new AlertDialog.Builder(GrabarVozActivity.this)
+                .setTitle("Grabar Voz")
+                .setMessage("Presione para grabar, suelte para terminar la grabación")
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
 
